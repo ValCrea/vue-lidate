a9554d8d441bcf94c7e8227d41ce9179ce324a0e
ValCrea <val.waldner@creatim.com>
2022-10-04 13:06:13 +0200

    Dynamic inputs

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,35 @@
-<template>Vuelidate</template>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+
+const inputs = { input1: ref(""), input2: ref("") };
+const rules = computed(() => {
+  const rules: any = {};
+
+  Object.keys(inputs).forEach((key) => {
+    rules[key] = {
+      required,
+    };
+  });
+
+  return rules;
+});
+const v$ = useVuelidate(rules, inputs);
+
+const submit = async () => {
+  if (await v$.value.$validate()) console.log("Valid");
+  else console.log("Invalid");
+};
+</script>
+
+<template>
+  <div v-for="(_, key) in inputs" :key="key">
+    <input type="text" v-model="inputs[key].value" />
+    <p>{{ v$[key].$dirty }} {{ v$[key].$invalid }} {{ v$[key].$error }}</p>
+    <br />
+  </div>
+  <button @click="submit">Submit</button>
+</template>
+
+<style scoped lang="scss"></style>
